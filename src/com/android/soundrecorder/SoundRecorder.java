f4b14f820b4496009a52d1b6f3a318294a87e343
wjiang <wjiang@codeaurora.org>
2013-12-27 10:12:35 +0800

    SoundRecorder: Activate a menu after starting/stopping recording
    On Android 3.0 and higher, the option menu is considered to
    be opened when menu item is presented in the action bar.
    Call invalidateOptionsMenu to request that the system call
    onPrepareOptionsMenu to update a menu.
    
    Change-Id: Id081bcbd70fc7cd670f411ae23d075a71bfe44c1

--- src/com/android/soundrecorder/SoundRecorder.java
+++ src/com/android/soundrecorder/SoundRecorder.java
@@ -643,6 +643,7 @@ public class SoundRecorder extends Activity
                                 mRecorder.sampleFile(), mMaxFileSize);
                     }
                 }
+                invalidateOptionsMenu();
                 break;
             case R.id.playButton:
                 if (!mRecorder.sampleFile().exists()) {
@@ -660,6 +661,7 @@ public class SoundRecorder extends Activity
                 mStateMessage2.setText(getResources().getString(R.string.recording_stopped));
                 mStateLED.setVisibility(View.VISIBLE);
                 mRecorderStop = true;
+                invalidateOptionsMenu();
                 break;
             case R.id.acceptButton:
                 if (!mRecorder.sampleFile().exists()) {
