9dc127ab55a17c783217dbcafaedb9d5f1041cb0
kaiyiz <kaiyiz@codeaurora.org>
2014-08-07 09:46:15 +0800

    Fix sound recorder force close
    During monkey test, there's possibility that sample file is
    deleted before clicking play button.
    
    Add null pointer check to avoid app force close.
    
    CRs-Fixed: 704623
    
    Change-Id: I635601877289b0d6d2cfc9de9d409fea646ad768

--- src/com/android/soundrecorder/SoundRecorder.java
+++ src/com/android/soundrecorder/SoundRecorder.java
@@ -670,7 +670,7 @@ public class SoundRecorder extends Activity
                 invalidateOptionsMenu();
                 break;
             case R.id.playButton:
-                if (!mRecorder.sampleFile().exists()) {
+                if (mRecorder.sampleFile() == null || !mRecorder.sampleFile().exists()) {
                     Toast.makeText(SoundRecorder.this, R.string.file_deleted,
                             Toast.LENGTH_SHORT).show();
                     finish();
@@ -687,7 +687,7 @@ public class SoundRecorder extends Activity
                 invalidateOptionsMenu();
                 break;
             case R.id.acceptButton:
-                if (!mRecorder.sampleFile().exists()) {
+                if (mRecorder.sampleFile() == null || !mRecorder.sampleFile().exists()) {
                     Toast.makeText(SoundRecorder.this, R.string.file_deleted,
                             Toast.LENGTH_SHORT).show();
                     finish();
